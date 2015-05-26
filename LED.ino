17523a0098f3d8d5d2cfd75f0ef5fefec6e65eee
littlepandabear <lpb.rianne@gmail.com>
2015-05-26 15:07:23 -0600

    Update LED.ino

--- LED.ino
+++ LED.ino
@@ -3,19 +3,19 @@
   Turns on LED for one second, then off for one second, repeatedly.
 */
  
-// Digital Pin 8 on Satellite Pin 3 via Port A
-int s3A = 8;
+// Digital Pin 6 on Satellite Pin 5 via Port A
+int s5A = 6;
 
 // the setup routine runs once when you press reset:
 void setup() {                
   // initialize the digital pin as an output.
-  pinMode(s3A, OUTPUT);     
+  pinMode(s5A, OUTPUT);     
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
-  digitalWrite(s3A, HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(s5A, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(1000);               // wait for a second
-  digitalWrite(s3A, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(s5A, LOW);    // turn the LED off by making the voltage LOW
   delay(1000);               // wait for a second
 }
