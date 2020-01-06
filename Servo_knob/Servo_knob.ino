3ebc052cacced06931fa0210ce2b0d214b886643
Rianne <9632612+littlepandabear@users.noreply.github.com>
2020-01-06 15:51:02 -0700

    Update Servo_knob.ino

--- Servo_knob/Servo_knob.ino
+++ Servo_knob/Servo_knob.ino
@@ -19,7 +19,7 @@ void setup()
  
 void loop() 
 { 
-  val = analogRead(potpin);            // reads the value of the potentiometer (value between 0 and 1023) 
+  val = analogRead(s1A);            // reads the value of the potentiometer (value between 0 and 1023) 
   val = map(val, 0, 1023, 0, 180);     // remap to use it with the servo (value between 0 and 180) 
   servo.write(val);                  // sets the servo position according to the scaled value 
   delay(15);                           // waits for the servo to get there 
